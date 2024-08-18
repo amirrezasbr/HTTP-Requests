af1556d147f10d49ace3a466ef099f5f12cb3d87
unknown <amirreza13.7676@gmail.com>
2024-08-18 12:40:10 +0330

    connecting buttons to fetch and send posts

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,7 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
+const form = document.querySelector("#new-post form");
+const fetchButton = document.querySelector("#available-posts button");
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
@@ -42,5 +44,11 @@ async function createPost(title, content) {
   sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
 }
 
-createPost("dummy", "a dummy content");
-fetchPosts();
+fetchButton.addEventListener("click", fetchPosts);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const enteredTitle = event.currentTarget.querySelector("#title").value;
+  const enteredcontent = event.currentTarget.querySelector("#content").value;
+
+  createPost(enteredTitle, enteredcontent);
+});
