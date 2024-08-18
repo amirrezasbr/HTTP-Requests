ac4fcda947969d5e9be3cd762e52a02d988447cd
unknown <amirreza13.7676@gmail.com>
2024-08-18 23:04:43 +0330

    handling errors

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -27,28 +27,40 @@ function sendHttpRequest(method, url, data) {
     headers: {
       "Content-type": "application/json",
     },
-  }).then((response) => {
-    return response.json();
-  });
+  })
+    .then((response) => {
+      if (response.status >= 200 && response.status < 300) {
+        return response.json();
+      } else {
+        return response.json().then((errData) => {
+          console.log(errData);
+          throw new Error("something went wrong. server side.");
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      throw new Error("something went wrong!");
+    });
 }
 
 async function fetchPosts() {
-  // try {
-  const responseData = await sendHttpRequest(
-    "GET",
-    "https://jsonplaceholder.typicode.com/posts"
-  );
-  const listOfPosts = responseData;
-  for (const post of listOfPosts) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector("h2").textContent = post.title.toUpperCase();
-    postEl.querySelector("p").textContent = post.body;
-    postEl.querySelector("li").id = post.id;
+  try {
+    const responseData = await sendHttpRequest(
+      "GET",
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    const listOfPosts = responseData;
+    for (const post of listOfPosts) {
+      const postEl = document.importNode(postTemplate.content, true);
+      postEl.querySelector("h2").textContent = post.title.toUpperCase();
+      postEl.querySelector("p").textContent = post.body;
+      postEl.querySelector("li").id = post.id;
 
-    listElement.append(postEl);
-    //   }
-    // } catch (error) {
-    //   alert(error.message);
+      listElement.append(postEl);
+    }
+  } catch (error) {
+    alert(error.message);
   }
 }
 
